f88fe6213c7133b600976e0640aad96f4f8e77ee
Nnyo <84756769+Nnyo@users.noreply.github.com>
2022-12-15 00:23:14 -0600

    Update logout_functions.js

--- js/function/logout_functions.js
+++ js/function/logout_functions.js
@@ -8,13 +8,13 @@ const logout = async ()=>{
 const loadMenu = async ()=>{
     var menuByRol = '';
     if(localStorage.getItem('role') == 'Admin'){
-        menuByRol = '<a href="/views/Administrador/almacen.html" class="nav-item nav-link">Almacen</a>'+
-                    '<a href="/views/Administrador/cadenas.html" class="nav-item nav-link">Cadenas</a>'+
-                    '<a href="/views/Administrador/donaciones.html" class="nav-item nav-link">Donaciones</a>'+
-                    '<a href="/views/Administrador/productos.html" class="nav-item nav-link">Productos</a>'+
-                    '<a href="/views/Administrador/usuarios.html" class="nav-item nav-link">Usuarios</a>';
+        menuByRol = '<a href="https://nnyo.github.io/sinara.github.io/views/Administrador/almacen.html" class="nav-item nav-link">Almacen</a>'+
+                    '<a href="https://nnyo.github.io/sinara.github.io/views/Administrador/cadenas.html" class="nav-item nav-link">Cadenas</a>'+
+                    '<a href="https://nnyo.github.io/sinara.github.io/views/Administrador/donaciones.html" class="nav-item nav-link">Donaciones</a>'+
+                    '<a href="https://nnyo.github.io/sinara.github.io/views/Administrador/productos.html" class="nav-item nav-link">Productos</a>'+
+                    '<a href="https://nnyo.github.io/sinara.github.io/views/Administrador/usuarios.html" class="nav-item nav-link">Usuarios</a>';
     }else if(localStorage.getItem('role') == 'Recolector'){
-        menuByRol = '<a href="/index.html" class="nav-item nav-link active">Inicio</a>';
+        menuByRol = '<a href="https://nnyo.github.io/sinara.github.io/index.html" class="nav-item nav-link active">Inicio</a>';
     }
     document.getElementById('containerMenu').innerHTML =
         '<div class="top-bar row gx-0 align-items-center d-none d-lg-flex">'+
@@ -31,7 +31,7 @@ const loadMenu = async ()=>{
         '</div>'+
         '<nav class="navbar navbar-expand-lg navbar-light py-lg-0 px-lg-5 wow fadeIn" data-wow-delay="0.1s">'+
             '<a href="#" class="navbar-brand ms-4 ms-lg-0">'+
-                '<img src="/images/SINARA.ico" width="30%" alt="">'+
+                '<img src="https://nnyo.github.io/sinara.github.io/images/SINARA.ico" width="30%" alt="">'+
             '</a>'+
             '<button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">'+
                 '<span class="navbar-toggler-icon"></span>'+
